ed1c13e0d39660b4725a8a94c2c00f82b3eaad7f
Hernan Velasquez <hernanudea@gmail.com>
2016-06-10 22:39:17 -0500

    Se usan fragment

---
+++ app/src/main/java/edu/gallileo/android/tipcalc/fragments/TipHistoryListFragmentListener.java
@@ -0,0 +1,9 @@
+package edu.gallileo.android.tipcalc.fragments;
+
+/**
+ * Created by HernánDarío on 10/06/2016.
+ */
+public interface TipHistoryListFragmentListener {
+    void action (String str);
+
+}
